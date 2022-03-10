f9c08b743bfd7186c07a71a9a031898fba523116
JigneshPurabiya26 <jignesh26purabiya@gmail.com>
2022-03-10 06:55:20 +0530

    Added the variable id to be used as a parameter

--- portfoliowebs/src/components/BlogCard.js
+++ portfoliowebs/src/components/BlogCard.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 export default function BlogCard(props) {
-    const{title,excerpt,imageUrl}=props.blog;
+    const{id,title,excerpt,imageUrl}=props.blog;
     return (
         <div className="card boxShadow h-100">
         <img className="card-image-top" src={imageUrl} alt="" />
         <div className="card-body">
           <h4 className="card-title">{title}</h4>
           <p className="card-text">{excerpt}</p>
-          <Link to="/blog/:id" className="stretched-link"></Link>
+          <Link to={`/blog/${id}`} className="stretched-link"></Link>
         </div>
       </div>
     )
